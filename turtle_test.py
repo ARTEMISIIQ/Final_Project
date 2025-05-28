cdfc2a02dcf7c651ebe7a9d0a43cb5b0719abb39
ARTEMISIIQ <etran360@gmail.com>
2025-05-27 23:37:34 -0400

    Add files via upload

--- turtle_test.py
+++ turtle_test.py
@@ -2,26 +2,51 @@
 print("Content-type: text/html \n")
 import cgi
 data = cgi.FieldStorage()
-def x():
-    t = ""
+
+if data.getvalue('x') != None:
+    x=int(data.getvalue('x'))
+    y=int(data.getvalue('y'))
+else:
     x = 0
     y = 0
-    while y < 30:
-        x += 1
-        if x > 100:
-            x = 0
+
+def plane():
+    t = ""
+    x0 = 0
+    y0 = 0
+    while y0 < 30:
+        x0 += 1
+        if x0 > 100:
+            x0 = 0
             t += "\n"
-            y += 1
-        t += "x"
+            y0 += 1
+        if x0 == x and y0 == y:
+            t += "O"
+        else:
+            t += "X"
     return t
-print(x(), "\n")
+print(plane(), "\n")
 def button():
     print('''
 \n
-<select name="">
-<option value="Up">Up</option>
-<option value="Right">Right</option>
-<option value="Down">Down</option>
-<option value="Left">Left</option>
-</select>''')
+<form action="../cgi-bin/turtle_test.py" method="GET">
+<select name="e" onchange="document.location.href=this.value">
+<option value=''' + "?x=" + str(x) + "&y=" + str(y) + "&dir=None"+ '''>Choose</option>
+<option value=''' + "?x=" + str(x) + "&y=" + str(y - 1) + "&dir=90" + '''>Up</option>
+<option value=''' + "?x=" + str(x + 1) + "&y=" + str(y) + "&dir=0"+'''>Right</option>
+<option value=''' + "?x=" + str(x) + "&y=" + str(y + 1) + "&dir=-90"+ '''>Down</option>
+<option value=''' + "?x=" + str(x - 1) + "&y=" + str(y) + "&dir=180"+ '''>Left</option>
+</select>
+</form>''')
 button()
+print(data.getvalue('x'),data.getvalue('y'))
+while 0<1:
+    if data.getvalue('dir') != None:
+        if data.getvalue('dir') == "90":
+            y -= 1
+        elif data.getvalue('dir') == "0":
+            x += 1
+        elif data.getvalue('dir') == "-90":
+            y += 1
+        else:
+            x -= 1
